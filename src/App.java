6e852e622a04c8349adf8dc79ea4257cbf2d0b99
AscCrs <cristoe2003@gmail.com>
2024-05-26 00:41:24 -0600

    Modificaciones en el archivo principal

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+// import java.io.BufferedReader;
+// import java.io.FileNotFoundException;
+// import java.io.FileReader;
+// import java.util.logging.Level;
+// import java.util.logging.Logger;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // try {
+        //     Gramatica parser = new Gramatica(new BufferedReader(new FileReader("./entrada.txt")));
+        //     parser.Analizar();
+        // } catch (ParseException e) {
+        //     System.err.println(e.getMessage());
+        // } catch (FileNotFoundException e) {
+        //     Logger.getLogger(ProyectoJavaCC.class.getName()).log(Level.SEVERE, "Error al intentar leer el archivo.", e);
+        // } catch(TokenMgrError e){
+        //     System.err.println(e.getMessage());
+        // }
     }
 }
