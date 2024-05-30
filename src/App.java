1ba73ee8bafcce9cc7b27f3546c1f18a8aa3b93c
AscCrs <cristoe2003@gmail.com>
2024-05-30 01:37:45 -0600

    Se corrigio la clase principal

--- src/App.java
+++ src/App.java
@@ -1,23 +1,25 @@
-// import java.io.BufferedReader;
-// import java.io.FileNotFoundException;
-// import java.io.FileReader;
-// import java.util.logging.Level;
-// import java.util.logging.Logger;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import Analizador.*;
 
 public class App {
      /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // try {
-        //     Gramatica parser = new Gramatica(new BufferedReader(new FileReader("./entrada.txt")));
-        //     parser.Analizar();
-        // } catch (ParseException e) {
-        //     System.err.println(e.getMessage());
-        // } catch (FileNotFoundException e) {
-        //     Logger.getLogger(ProyectoJavaCC.class.getName()).log(Level.SEVERE, "Error al intentar leer el archivo.", e);
-        // } catch(TokenMgrError e){
-        //     System.err.println(e.getMessage());
-        // }
+        try {
+            GolangParser parser = new GolangParser(new BufferedReader(new FileReader("./entrada.go")));
+            parser.CompilationUnit();
+            System.out.println("Parsing completed successfully.");
+        } catch (ParseException e) {
+            System.err.println("Token Error: " + e.getMessage());
+        } catch (FileNotFoundException e) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, "Error al intentar leer el archivo.", e);
+        } catch(TokenMgrError e){
+            System.err.println("Lexical error: " + e.getMessage());
+        }
     }
 }
